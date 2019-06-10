e111c5ffa05f9525b798668aa1b8dff7fb10235d
eureka84 <angelosciarra@ymail.com>
2019-06-10 23:38:41 +0200

    initial commit

---
+++ src/main/kotlin/com/eureka/bankkata/Account.kt
@@ -0,0 +1,14 @@
+package com.eureka.bankkata
+
+class Account {
+
+    fun deposit(amount: Int){
+        TODO()
+    }
+    fun withdraw(amount: Int){
+        TODO()
+    }
+    fun printStatement(){
+        TODO()
+    }
+}
